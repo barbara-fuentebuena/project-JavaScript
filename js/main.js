05862f2956771e58ba9354477ad7bd4c10091c4c
barbaraf <barbi.fb@hotmail.com>
2023-04-29 13:04:45 +0200

    DOM included, calculator working

--- js/main.js
+++ js/main.js
@@ -1,280 +1,133 @@
-// SECTION 1 - LOG IN
 
-//create user name and log in
+//render calculate button to calculate the IBM
+const calculateButton = document.getElementById("calculateButton");
 
-let userName = ""
+//action when the calculate button is clicked
+calculateButton.addEventListener("click", ()=>{
+    
+    //get values - DOM
+    let emailUser = document.getElementById("emailUser").value
+    let gender = document.querySelector("#genderInput").value
+    const age = parseInt(document.getElementById("ageInput").value)
+    const height = parseInt(document.getElementById("heightInput").value)
+    const weight = parseInt(document.getElementById("weightInput").value)
+    let activityLevel = document.querySelector("#activityLevelInput").value
 
-function Login(){
-    do{
-        userName = prompt("SIGN UP and calculate your calorie deficit!\n\nCreate a user name:");
-    }while((userName == "") || (userName == null))
-    let password = function(){
-        let selectPassword = prompt("Create a password:");
-        let repeatPassword = prompt("Repeat password:");
-        if((selectPassword == repeatPassword) && (selectPassword != "")){
-            alert(`Hi ${userName}, welcome to our calculator!`);
-            start();
-        }else if((selectPassword != repeatPassword) && (selectPassword != "")){
-            alert("Incorrect password. Try again.");
-            password();
-        }else if(selectPassword == ""){
-            alert("You must enter a password to create your account. Try again.");
-            password();
+    //initialize isValid to true
+    let isValid = true; 
+
+    //clear previous messages:
+    //error menssage:
+    const errorMessage = document.getElementById("errorMessage");
+    errorMessage.innerHTML = "";
+    //result message:
+    let message = document.getElementById("resultTotal")
+    message.innerHTML = ""
+
+    //rules for values
+    if((isNaN(age)) || ((age < 0)) || (age == null)){
+        isValid = false;
+        const errorMessage = document.createElement("p")
+        errorMessage.innerHTML = `
+        <p>* You must insert a positive number for age.</p>`
+        document.getElementById("errorMessage").appendChild(errorMessage)
+    }else if(age < 18){
+        isValid = false;
+        const errorMessage = document.createElement("p")
+        errorMessage.innerHTML = `
+        <p>* You must to be over 18 years old to access to the calculator.</p>`
+        document.getElementById("errorMessage").appendChild(errorMessage)
+    }else if((isNaN(weight)) || (weight < 0) || (weight == null)){
+        isValid = false;
+        const errorMessage = document.createElement("p")
+        errorMessage.innerHTML = `
+        <p>* You must insert a positive number for weight.</p>`
+        document.getElementById("errorMessage").appendChild(errorMessage)
+    }else if((isNaN(height)) || (height < 0) || (height == null)){
+        isValid = false;
+        const errorMessage = document.createElement("p")
+        errorMessage.innerHTML = `
+        <p>* You must insert a positive number for height.</p>`
+        document.getElementById("errorMessage").appendChild(errorMessage)
+    }else if(emailUser == "" || emailUser.includes("@" && ".") == false){
+        isValid = false;
+        const errorMessage = document.createElement("p")
+        errorMessage.innerHTML = `
+        <p>* You must insert a valid email.</p>`
+        document.getElementById("errorMessage").appendChild(errorMessage)   
+    }else if(gender == "none" || activityLevel == "none"){
+        isValid = false;
+        const errorMessage = document.createElement("p")
+        errorMessage.innerHTML = `
+        <p>* Make sure you select your gender and activity level.</p>`
+        document.getElementById("errorMessage").appendChild(errorMessage)   
+    }
+    errorMessage.className = "errorMessage"
+
+    //if there are no errors in the inputs:
+    if (isValid) {
+        //choose gender value
+        if(gender == "female"){
+            gender = -161
+        }else if(gender == "male"){
+            gender = 5
+        }else if(gender == "other"){
+            gender = -80
         }
-    }
-    password(); 
-}
-
-//SECTION 2 CALCULATOR
-
-//calorie deficit calculator for each gender
-const calculatorFemale = (age, weight, height, gender) =>{
-    let result = ((10 * weight) + (6.65 * height) - (5 * age) - gender);
-    return result
-}
-
-const calculatorMale = (age, weight, height, gender) =>{
-    let result = ((10 * weight) + (6.65 * height) - (5 * age) + gender);
-    return result
-}
-
-const calculatorOther = (age, weight, height, gender) =>{
-    let result = ((10 * weight) + (6.65 * height) - (5 * age) - gender);
-    return result
-}
-
-// function to choose activity level
-const chooseActivityLevel = () =>{
-    let resultActivityLevel = prompt("Select your activity level:\n\n1) Very Active\n2) Active\n3) Not too active\n4) Not active at all").toLocaleLowerCase();
-    if((resultActivityLevel == "1") || (resultActivityLevel == "very active")){
-        activityLevel = 500;
-    }else if((resultActivityLevel == "2") || (resultActivityLevel == "active")){
-        activityLevel = 300;
-    }else if((resultActivityLevel == "3") || (resultActivityLevel == "not too active")){
-        activityLevel = 150;
-    }else if((resultActivityLevel == "4") || (resultActivityLevel == "not active at all")){
-        activityLevel = 0;
-    }else{
-        alert("Invalid entry");
-        chooseActivityLevel();
-    } 
-}
-
-// function to select gender and create a value depending on the gender, in order to use the TBM calculator 
-const chooseGender = () =>{
-    let resultGender = prompt("Select your gender:\n\n1) Female\n2) Male\n3) Other").toLocaleLowerCase();
-    if((resultGender === "1") || (resultGender === "female") && (resultGender != "")){
-        gender = 161;
-    }else if((resultGender === "2") || (resultGender === "male") && (resultGender != "")){
-        gender = 5;
-    }else if((resultGender === "3") || (resultGender === "other") && (resultGender != "")){
-        gender = 80;
-    }else{
-        alert("Invalid entry. Try again.");
-        chooseGender();
-    }
-}
-
-let age = "";
-let weight = "";
-let height = "";
-
-const personalDetails = () =>{
-    chooseAge = () =>{
-        age = Number(prompt("Insert your age:"));
-        if((isNaN(age)) || ((age < 0))){
-            (alert("You must insert a positive number."));
-            chooseAge();
-        }else if(age < 18){
-            (alert("You must to be over 18 years old to access to the calculator."));
-            chooseAge();
+        //choose activity value
+        if(activityLevel == "1"){
+            activityLevel = 500
+        }else if(activityLevel == "2"){
+            activityLevel = 300
+        }else if(activityLevel == "3"){
+            activityLevel = 150
+        }else if(activityLevel == "4"){
+            activityLevel = 0
         }
-    }
-    chooseWeight = () =>{
-        weight = Number(prompt("Insert your weight in kilos:"));
-        if((isNaN(weight)) || (weight < 0)){
-            (alert("You must insert a positive number."));
-            chooseWeight();
-        }
-    }
-    chooseHeight = () =>{
-        height = Number(prompt("Insert your height in centimentres:"));
-        if((isNaN(height)) || (height < 0)){
-            (alert("You must insert a positive number."));
-            chooseHeight();
-        }
-    }
-    chooseAge()
-    chooseWeight()
-    chooseHeight();
-}
 
+        //calculator
+        let result = parseInt(((10 * weight) + (6.65 * height) - (5 * age) + gender)+activityLevel)
+        //show result
+        let message = document.getElementById("resultTotal")
+        message.innerHTML = `➡️ Your TBM is ${result}. It means that you should consume ${result} calories as maximum per day if you want to lose weight.`
+        const errorMessage = document.getElementById("errorMessage");
+        errorMessage.innerHTML = "";
+        message.className = "resultMessage"
+        //show reset button to reset the calculator
+        let resetButton = document.getElementById("resetButton")
+        resetButton.innerHTML = `RESET ↺`
+        resetButton.className = "resetButton"
 
-//main programm
-const start = function(){
-    const calculate = () =>{
-        if(gender == 161){
-            let resultTBM = confirm(`Your TBM is ${Math.round(calculatorFemale(age, weight, height, gender)+activityLevel)}. It means that you should consume ${Math.round(calculatorFemale(age, weight, height, gender)+activityLevel)} calories as maximum per day if you want to lose weight.\n\nWould you like to calculate it again?`); 
-            if(resultTBM == true){
-                start();
-            }else{
-                offerRecipe();
-            }
-        }else if(gender == 5){
-            let resultTBM = confirm(`Your TBM is ${Math.round(calculatorMale(age, weight, height, gender)+activityLevel)}. It means that you should consume ${Math.round(calculatorMale(age, weight, height, gender)+activityLevel)} calories as maximum per day if you want to lose weight.\n\nWould you like to calculate it again?`);
-            if(resultTBM == true){
-                start();
-            }else{
-                offerRecipe();
-            }
-        }else if(gender == 80){
-            let resultTBM = confirm(`Your TBM is ${Math.round(calculatorOther(age, weight, height, gender)+activityLevel)}. It means that you should consume ${Math.round(calculatorOther(age, weight, height, gender)+activityLevel)} calories as maximum per day if you want to lose weight.\n\nWould you like to calculate it again?`);
-            if(resultTBM == true){
-                start();
-            }else{
-                offerRecipe();
-            }
-        }else{
-            alert("Invalid entry.");
-            start();
-        }
-    }
-    chooseGender();
-    personalDetails();
-    chooseActivityLevel();
-    calculate();
-}
+        //clear input values after calculation
+        document.getElementById("emailUser").value = ""
+        gender.value = ""
+        document.getElementById("ageInput").value = ""
+        document.getElementById("heightInput").value = ""
+        document.getElementById("weightInput").value = ""
+        activityLevel.value = ""
 
-//SECTION 3 RECIPES
-
-
-
-//functions to creat recipes:
-
-class Recipe{
-    constructor(name, category, ingredients, calories, serving, protein, fats){
-        this.name = name
-        this.category = category
-        this.ingredients = ingredients
-        this.calories = calories
-        this.serving = serving
-        this.protein = protein
-        this.fats = fats
-    }
-}
-
-//recipes: 
-
-
-const quinoaBurger = new Recipe("Quinoa Burger", "Low Calories", "20g breadcrumbs, 2 eggs, 130g onions, 100g carrots, 310g zuccini, 200g white quinoa", 92, 12, 3.9, 2);
-
-const appleCrumble = new Recipe ("Apple Crumble", "Low Calories", "20g cornflour, 3 eggs, 200g natural sweetener, 400g plain protein yoghurt, 4 apples", 93, 8, 7.8, 2);
-
-const proteinCookies = new Recipe ("Protein Cookies", "High Protein", "5ml vanilla essence, 1 entire egg, 1 egg white, 25g dark choco chips, 20g cornflour, 40g natural sweetner, 80g peanut butter, 1,5 scoops vanilla whey protein", 153, 6, 10.2, 9.3);
-
-const greenPie = new Recipe ("Green Pie", "High Protein", "2 zuccini, 120g cottage cheese (fat free), 100g mushrooms, 100g onion, 120g light cheese, 170g asparagus, 115g brocoli, 4 eggs", 429, 4, 21.6, 6.5);
-
-const wholeWheatPancakes = new Recipe ("Whole Wheat Pancakes", "Low Fat", "75g whole wheat flour, 150ml almond milk", 74, 4, 2.7, 0.7);
-
-const spinachBurgers = new Recipe ("Spinach Burgers", "Low Fat", "1 egg, 100g cottage cheese, 1/2 capsicum, 100g carrots, 230g onion, 500g frozen spinach", 50, 10, 3.6, 1.2);
-
-
-//array of recipes:
-
-
-const recipes = [quinoaBurger, appleCrumble, proteinCookies, greenPie, wholeWheatPancakes, spinachBurgers]
-
-
-//function to offer a recipe according to user's condition
-
-const offerRecipe = function(){
-    let weeklyRecipe = confirm("Would you like to check our recipes of the week?");
-    if(weeklyRecipe == true){
-        const chooseCategory = () =>{
-            let chosenCategory = prompt("Which category do you prefer?\n\n1) Low calories\n2) High protein\n3) Low Fat").toLocaleLowerCase();
-            if((chosenCategory === "1") || (chosenCategory === "low calories" || (chosenCategory === "low calorie"))){
-                let askCalories = Number(prompt(`Which is the maximum of calories per serving you are looking for?`));
-                let condition = recipes.find((element)=>{return element.calories < askCalories});
-                if((!isNaN(askCalories)) && (askCalories > 0)){
-                    if(condition){
-                        let getRecipe = confirm(`We have the perfect recipe for you: ${condition.name} (${condition.serving} servings)\n\nIngredients:\n${condition.ingredients}\n\nTotal ${condition.calories} calories per serving.\n\nWould you like to come back to the category menu?`);
-                        if(getRecipe){
-                            chooseCategory();
-                        }else{
-                            alert(`See you next time ${userName}!`);
-                        }
-                    }else{
-                        let sorry = confirm(`Sorry, we don't have any recipe that reaches your expectations. Would you like to try again?`);
-                        if(sorry){
-                            chooseCategory();
-                        }
-                        else{
-                            alert(`See you next time ${userName}!`);
-                        }
-                    }
-                }else{
-                    alert(`Invalid Invalid entry. You must enter a positive number.`);
-                    chooseCategory();
-                }
-            }else if((chosenCategory === "2") || (chosenCategory === "high protein" || (chosenCategory === "high proteins"))){
-                let askProtein = Number(prompt(`Which is the miminum of protein per serving you are looking for?`));
-                let condition = recipes.find((element)=>{return element.protein > askProtein});
-                if((!isNaN(askProtein)) && (askProtein > 0)){
-                    if(condition){
-                        let getRecipe = confirm(`We have the perfect recipe for you: ${condition.name} (${condition.serving} servings)\n\nIngredients:\n${condition.ingredients}\n\nTotal ${condition.protein} protein per serving.\n\nWould you like to come back to the category menu?`);
-                        if(getRecipe){
-                            chooseCategory();
-                        }else{
-                            alert(`See you next time ${userName}!`);
-                        }
-                    }else{
-                        let sorry = confirm(`Sorry, we don't have any recipe that reaches your expectations. Would you like to try again?`);
-                        if(sorry){
-                            chooseCategory();
-                        }
-                        else{
-                            alert(`See you next time ${userName}!`);
-                        }
-                    }
-                }else{
-                    alert(`Invalid entry. You must enter a positive number.`);
-                    chooseCategory();
-                }
-            }else if((chosenCategory === "3") || (chosenCategory === "low fat" || (chosenCategory === "low fats"))){
-                let askFats = Number(prompt(`Which is the maximum of fats per serving you are looking for?`));
-                let condition = recipes.find((element)=>{return element.fats < askFats});
-                if((!isNaN(askFats)) && (askFats > 0)){
-                    if(condition){
-                        let getRecipe = confirm(`We have the perfect recipe for you: ${condition.name} (${condition.serving} servings)\n\nIngredients:\n${condition.ingredients}\n\nTotal ${condition.fats} fats per serving.\n\nWould you like to come back to the category menu?`);
-                        if(getRecipe){
-                            chooseCategory();
-                        }else{
-                            alert(`See you next time ${userName}!`);
-                        }
-                    }else{
-                        let sorry = confirm(`Sorry, we don't have any recipe that reaches your expectations. Would you like to try again?`);
-                        if(sorry){
-                            chooseCategory();
-                        }
-                        else{
-                            alert(`See you next time ${userName}!`);
-                        }
-                    }
-                }else{
-                    alert(`Invalid entry. You must enter a positive number.`);
-                    chooseCategory();
-                }
-            }else{
-                alert(`Invalid entry. Try again.`);
-                chooseCategory();
+        //create new user object
+        class User{
+            constructor(emailUser, gender, age, height, weight, activityLevel){
+                this.emailUser = emailUser;
+                this.gender = gender;
+                this.age = age;
+                this.height = height;
+                this.weight = weight;
+                this.activityLevel = activityLevel
             }
         }
-        chooseCategory();
-    }else{
-        alert(`See you next time ${userName}!`);
+        const newUser = new User (emailUser, gender, age, height, weight, activityLevel)
+
+        //add user object to usersArray
+        usersArray.push(newUser)
+
+        //save usersArray to localStorage
+        localStorage.setItem("usersArray", JSON.stringify(usersArray))
     }
-}
+})
 
+//get usersArray from localStorage 
+const usersArray = JSON.parse(localStorage.getItem("usersArray")) || []
 
-Login();
-
+console.log(usersArray)
